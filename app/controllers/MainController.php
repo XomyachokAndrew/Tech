d346a42d9f74949c1015e8a864bf33c664b0f891
Andrew <anki_06@mail.ru>
2024-04-14 11:50:56 +0300

    Add validation

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -14,10 +14,18 @@ class MainController extends Controller
 
     public function indexinsertaction()
     {
+        if (!isset($_POST['full_name']) || !isset($_POST['email']) || !isset($_POST['message'])) {
+            return;
+        }  
+
         $full_name = $_POST['full_name'];
         $email = $_POST['email'];
         $message = $_POST['message'];
+        $pattern_email = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/';
 
+        if (!preg_match($pattern_email, $email) || $full_name == null || $message == null) {
+            return;
+        }
         $array = [
             'full_name' => $full_name,
             'email' => $email,
--- app/views/index.php
+++ app/views/index.php
@@ -11,9 +11,9 @@
         <section class="left_column">
             <form id="message_form">
                 <h1 class="title_send">Форма отправки</h1>
-                <input type="text" name="full_name" id="fullname" placeholder="ФИО">
-                <input type="email" name="email" id="email" placeholder="Email">
-                <textarea name="message" id="message" cols="30" rows="10" placeholder="Сообщение"></textarea>
+                <input type="text" name="full_name" id="fullname" placeholder="ФИО" required>
+                <input type="email" name="email" id="email" placeholder="Email" required>
+                <textarea name="message" id="message" cols="30" rows="10" placeholder="Сообщение" required></textarea>
                 <input class="button_send" type="submit" value="Отправить">
             </form>
         </section>
